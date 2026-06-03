3fce464ad388dba30d4f06b9940037b8356dd641
kevinboudjok-maker <kevinboudjok@gmail.com>
2026-06-03 12:35:35 -0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,12 +1,52 @@
-<script>
-function login() {
-    const user = document.getElementById('username').value;
-    const pass = document.getElementById('password').value;
+  let users = JSON.parse(localStorage.getItem("users")) || [];
 
-    if (user === 'kevin' && pass === '1234') {
-        window.location.href = 'index.html';
-    } else {
-        alert('Identifiants incorrects');
-    }
-}
+document.getElementById("registerForm").addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const name = registerName.value;
+  const email = registerEmail.value;
+  const password = registerPassword.value;
+  const confirm = registerConfirm.value;
+  const error = document.getElementById("registerError");
+
+  if (!name || !email || !password || !confirm) {
+    error.textContent = "Tous les champs sont obligatoires";
+    return;
+  }
+
+  if (!email.includes("@")) {
+    error.textContent = "Email invalide";
+    return;
+  }
+
+  if (password !== confirm) {
+    error.textContent = "Les mots de passe ne correspondent pas";
+    return;
+  }
+
+users.push({ name, email, password });
+  localStorage.setItem("users", JSON.stringify(users));
+
+  localStorage.setItem("isLoggedIn", "true");
+  localStorage.setItem("currentUser", email);
+
+  window.location.href = "html/iindex.html";
+});
+
+document.getElementById("loginForm").addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const email = loginEmail.value;
+  const password = loginPassword.value;
+  const error = document.getElementById("loginError");
+
+  const user = users.find(
+    u => u.email === email && u.password === password
+  );
+
+  if (!user) {
+    error.textContent = "Email ou mot de passe incorrect";
+    return;
+  }
+
 </script>
